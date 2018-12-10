5418cf2e9c9214615ad15c0b8bec8f6e8bfc991e
Gabriel Brun <lydstyl@gmail.com>
2018-12-10 23:10:26 +0100

    Mise en production sur Pi

--- src/radar.js
+++ src/radar.js
@@ -4,16 +4,16 @@ var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'radar',
     data: {
-        labels: ['Javascript / Jquery', 'Vue.js', 'CSS / Sass', 'Node.js', 'Python / Django', 'PHP / Codeigniter', 'Git'],
+        labels: ['Javascript / Jquery', 'Vue.js ou React.js', 'CSS / Sass', 'Node.js', 'Python / Django', 'PHP / Codeigniter', 'Git'],
         datasets: [
             {
                 label: 'aujourd\'hui',
-                data: [3.5, 0, 4, 2, 2.5, 3, 5],
+                data: [3.5, 0, 4, 3.5, 2.5, 3, 5],
                 backgroundColor: 'rgba(40, 167, 69, 0.3)'
             },
             {
                 label: 'objectifs',
-                data: [4.5, 3, 4.5, 4, 3.5, 3, 5],
+                data: [4, 4.5, 4.5, 4.5, 3.5, 3.5, 5],
                 backgroundColor: 'rgba(40, 167, 69, 0.1)'
             }
         ]
